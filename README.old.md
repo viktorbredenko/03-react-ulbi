a0f967c19f23388b912a979dc26a949f47f023a0
viktorbredenko <viktorbredenko@gmail.com>
2022-11-21 16:48:29 +0200

    2111221648

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# 03-react-ulbi
+# https://www.youtube.com/watch?v=GNrdg3PzpJQ
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  let likes = 5;
+  return (
+    <div className="App">
+      <h1>{likes}</h1>
+      <button onClick={()=> likes += 1}>Increment</button>
+      <button onClick={()=> likes -= 1}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
